d50f0aa3ff3cc6c23aa58624260a0f52bfa9449e
TNTxTrick <139092950+TNTxTrick@users.noreply.github.com>
2024-02-04 06:29:40 +0000

    Initial commit

---
+++ public/up_dowload/twinfo.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+exports.name = '/twinfo';
+
+exports.index = async (req, res, next) => {
+  const user = req.query.user;
+  if (!user) {
+    return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+  }
+
+  const options = {
+    method: 'GET',
+    url: 'https://twitter-api45.p.rapidapi.com/screenname.php',
+    params: { screenname: user },
+    headers: {
+      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+      'X-RapidAPI-Host': 'twitter-api45.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    if (response.status === 200) {
+      console.log(response.data);
+      return res.json(response.data);
+    } else {
+      throw new Error('Có lỗi xảy ra khi tải dữ liệu từ Twitter API');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải dữ liệu từ Twitter API' });
+  }
+};
---
+++ public/up_dowload/ytbshort.js
@@ -0,0 +1,26 @@
+exports.name = '/ytbshort';
+exports.index = async (req, res, next) => {
+  const link = req.query.link;
+  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+
+  const axios = require('axios');
+
+  const options = {
+    method: 'GET',
+    url: 'https://youtube-video-and-shorts-downloader1.p.rapidapi.com/api/getYTVideo',
+    params: { url: link },
+    headers: {
+      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d', 
+      'X-RapidAPI-Host': 'youtube-video-and-shorts-downloader1.p.rapidapi.com'
+    }
+  };
+
+  try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Youtube' });
+    }
+  };
