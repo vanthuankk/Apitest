442013923e98cc411d27aa62dde77aa9460907d6
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-02-09 13:02:05 +0700

    Create threadsinfo.js

---
+++ public/up_dowload/threadsinfo.js
@@ -0,0 +1,25 @@
+exports.name = '/threads/infouser';
+exports.index = async(req, res, next) => {
+const keyword = req.query.keyword;
+if (!keyword) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://threads-by-meta-threads-an-instagram-app-detailed.p.rapidapi.com/get_user',
+  params: {user_id: keyword},
+  headers: {
+    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+    'X-RapidAPI-Host': 'threads-by-meta-threads-an-instagram-app-detailed.p.rapidapi.com'
+  }
+};
+
+  try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: 'Có lỗi xảy ra khi tải thông tin từ API' });
+    }
+  };
