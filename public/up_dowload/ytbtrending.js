8e65a20403a19134f1f069ffbc81eeb95005f8b4
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-02-15 21:15:45 +0700

    Create ytbtrending.js

---
+++ public/up_dowload/ytbtrending.js
@@ -0,0 +1,20 @@
+exports.name = '/ytb/trending';
+exports.index = async (req, res, next) => {
+  const keyword = req.query.keyword;
+  if (!keyword) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/trending',
+  params: {
+    type: keyword,
+    hl: 'en',
+    gl: 'VN'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
