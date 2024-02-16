24735dcf01c07ff03d9fa889b3d631a18eaea317
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-02-16 12:14:04 +0700

    Update aivoicetext.js

--- public/up_dowload/aivoicetext.js
+++ public/up_dowload/aivoicetext.js
@@ -7,23 +7,23 @@ exports.index = async (req, res, next) => {
 
   const axios = require('axios');
 
-  const options = {
-    method: 'POST',
-    url: 'https://ai-voice-text-to-speach.p.rapidapi.com/makevoice',
-    params: {
-      text: keyword,
-      voice: 'm1'
-    },
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-      'X-RapidAPI-Host': 'ai-voice-text-to-speach.p.rapidapi.com'
-    },
-    data: {
-      key1: 'value',
-      key2: 'value'
-    }
-  };
+const options = {
+  method: 'POST',
+  url: 'https://tts79.p.rapidapi.com/tts/elevenlabs',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+    'X-RapidAPI-Host': 'tts79.p.rapidapi.com'
+  },
+  data: {
+    model_id: 'eleven_multilingual_v2',
+    voice_id: '21m00Tcm4TlvDq8ikWAM',
+    text: keyword,
+    similarity_boost: 0.75,
+    stability: 0.5,
+    use_speaker_boost: true
+  }
+};
 
   try {
     const response = await axios.request(options);
