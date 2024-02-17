34d2880ed41ed1fd7a4078e4724be856c95dd677
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-02-17 17:37:52 +0700

    Create gai.js

---
+++ public/images/gai.js
@@ -0,0 +1,14 @@
+exports.name = '/images/girl';
+exports.index = async(req, res, next) => {
+    try {
+        const girl = require('./data/json/girl.json');
+        var image = girl[Math.floor(Math.random() * girl.length)].trim();
+        res.jsonp({
+            url: image,
+            count: girl.length,
+            author: 'Tnt'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}
