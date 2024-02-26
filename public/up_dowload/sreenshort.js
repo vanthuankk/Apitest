1cc328cf332e00298f64c1e3e2683f3e4ab0b856
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-02-26 12:34:06 +0700

    Create sreenshort.js

---
+++ public/up_dowload/sreenshort.js
@@ -0,0 +1,35 @@
+exports.name = '/sreenshort';
+exports.index = async (req, res, next) => {
+  const { uid, cookies } = req.query;
+  if (!uid || !cookies) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' });
+
+  const axios = require('axios');
+
+  const options = {
+    method: 'GET',
+    url: `https://facebook.com/${uid}/`,
+    headers: {
+      'authority': 'business.facebook.com',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'accept-language': 'en-US,en;q=0.9',
+      'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': "Windows",
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'Cookie': cookies
+    }
+  };
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Facebook' });
+  }
+};
