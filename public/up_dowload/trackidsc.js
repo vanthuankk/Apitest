60a0f4d51cacec53888742baf2dc4f404511a454
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-03 11:18:31 +0700

    Update trackidsc.js

--- public/up_dowload/trackidsc.js
+++ public/up_dowload/trackidsc.js
@@ -1,11 +1,14 @@
-
+exports.name = '/trackidsc';
+exports.index = async (req, res, next) => {
+  const keyword = req.query.keyword;
+  if (!keyword) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình' }); 
 const axios = require('axios');
 
 const options = {
   method: 'GET',
   url: 'https://spotify-scraper.p.rapidapi.com/v1/track/search',
   params: {
-    name: 'thien ly oiw'
+    name: keyword
   },
   headers: {
     'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
