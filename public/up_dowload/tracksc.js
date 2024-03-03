b9c1835e25b07d47a0474d8d113ceb42241981ae
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-03-03 09:47:20 +0700

    Update tracksc.js

--- public/up_dowload/tracksc.js
+++ public/up_dowload/tracksc.js
@@ -16,3 +16,12 @@ const options = {
   }
 };
 
+try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      return res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      return res.json({ error: 'Có lỗi xảy ra khi tải từ API' });
+    }
+  };
