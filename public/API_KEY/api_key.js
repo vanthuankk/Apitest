e0d174d7de55ffbf9de4c31ca9f75ad6208d7da2
vanthuankk <118978508+vanthuankk@users.noreply.github.com>
2024-03-09 22:19:06 +0700

    Update api_key.js

--- public/API_KEY/api_key.js
+++ public/API_KEY/api_key.js
@@ -18,7 +18,7 @@ exports.index = async(req, res, next) => {
             error: 'Thiếu dữ liệu để thực hiện yêu cầu cho bạn'
         });
         else {
-            if (req.query.apikey == 'TNTXTRICK_170507') {
+            if (req.query.apikey == 'TNTXTRICK_170508') {
                 var type = 'premium';
                 var apikey = 'TNTXTRICK_';
                 var request = 'infinite';
