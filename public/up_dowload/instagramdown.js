5e928321f18806dcf3ae9c0f770f361611b7a09f
vanthuankk <118978508+vanthuankk@users.noreply.github.com>
2024-03-10 09:18:54 +0700

    Update instagramdown.js

--- public/up_dowload/instagramdown.js
+++ public/up_dowload/instagramdown.js
@@ -1,26 +1,29 @@
-exports.name = '/instagramdown';
+exports.name = '/downinsta';
 exports.index = async (req, res, next) => {
   const link = req.query.link;
-  if (!link) return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
+  if (!link) {
+    return res.status(400).json({ error: 'Thiếu đường dẫn Instagram để tải xuống' });
+  }
+
   const axios = require('axios');
 
   const options = {
-    method: 'POST',
-    url: 'https://all-video-downloader1.p.rapidapi.com/Instagram',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
-      'X-RapidAPI-Host': 'all-video-downloader1.p.rapidapi.com'
+    method: 'GET',
+    url: 'https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index',
+    params: {
+      url: link
     },
-    data: { url: link }
+    headers: {
+      'X-RapidAPI-Key': 'fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d',
+      'X-RapidAPI-Host': 'instagram-downloader-download-instagram-videos-stories.p.rapidapi.com'
+    }
   };
 
   try {
     const response = await axios.request(options);
-    console.log(response.data);
     return res.json(response.data);
   } catch (error) {
-    console.error(error);
-    return res.json({ error: 'Có lỗi xảy ra khi tải xuống dữ liệu từ Instagram' });
+    console.error('Lỗi khi tải từ API:', error);
+    return res.status(500).json({ error: 'Có lỗi xảy ra khi tải từ API' });
   }
 };
