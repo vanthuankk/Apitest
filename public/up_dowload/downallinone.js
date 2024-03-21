b3108a89f26c6c3363a9a86cfbd588d885a96905
vanthuankk <118978508+vanthuankk@users.noreply.github.com>
2024-03-21 22:27:01 +0700

    Update downallinone.js

--- public/up_dowload/downallinone.js
+++ public/up_dowload/downallinone.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const keyAPi = ['fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d','d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','b619707d57mshc1e2f8dec3870ecp12e04cjsnb42dfa0c28ca','c320de0ac9msh8c90df54d8babafp144ae9jsn419b1c1a8f14']
+const keyAPi = ['fd92cf57c9msh1f7b78b804353c7p1548f3jsn69db0304865d','d0ab76bc06msh5032ca2f6f3baf9p15f9d8jsn024d834a55cb','b619707d57mshc1e2f8dec3870ecp12e04cjsnb42dfa0c28ca']
  var keyRandom = keyAPi[Math.floor(Math.random() * keyAPi.length)];
 
 exports.name = '/downall';
